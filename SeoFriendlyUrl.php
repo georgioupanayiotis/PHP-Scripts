7469d851f4ff245e54bb73b255e8c8b4fc4e2675
Panayiotis Georgiou <info@panayiotisgeorgiou.net>
2015-03-10 10:30:18 +0000

    seofriendlyurl function

---
+++ SeoFriendlyUrl.php
@@ -0,0 +1,17 @@
+/**
+  * Return an SEO-friendly URL-Friendly
+  * @param string $string
+  * @return string
+  */
+
+function seoFriendlyUrl($string) {
+    //Unwanted:  {UPPERCASE} ; / ? : @ & = + $ , . ! ~ * ' ( )
+    $string = strtolower($string);
+    //Strip any unwanted characters
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    //Clean multiple dashes or whitespaces
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    //Convert whitespaces and underscore to dash
+    $string = preg_replace("/[\s_]/", "-", $string);
+    return $string;
+}
